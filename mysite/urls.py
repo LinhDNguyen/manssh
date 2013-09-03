29a9dce039473e6effb7fd43e39c1b85bf7fdc1a
Nguyen Van Linh <nvl1109@gmail.com>
2013-09-03 20:22:16 +0700

    Update relative path for URL and turn DEBUG off.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
 from manssh import views
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
@@ -22,9 +16,9 @@ urlpatterns = patterns('',
     url(r'^manssh/saveKey.*$', views.saveKey, name='index'),
     url(r'^manssh/deleteKey.*$', views.deleteKey, name='index'),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        { 'document_root': '/home/linh/temp/mysite/static',
+        { 'document_root': settings.STATIC_ROOT,
           'show_indexes': True }),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        { 'document_root': '/home/linh/temp/mysite/media',
+        { 'document_root': settings.MEDIA_ROOT,
           'show_indexes': True }),
 )
